9269ac57797fef617bd6e63db960700328698e00
lanasladovic <lanasladovic1@gmail.com>
2018-01-08 07:10:32 +0100

    initial comm

---
+++ web_p.js
@@ -0,0 +1,13 @@
+/*var box1 = document.getElementsByClassName('box1');
+box1.animate([
+    {transform: 'scale(1)', opacity: 1, offset: 0},
+    {transform: 'scale(.5) rotate(270deg)', opacity: .5, offset: .2},
+    {transform: 'scale(1) rotate(0deg)', opacity: 1, offset: 1},],
+            {
+    duration: 2000,
+    easing: 'ease-in-out',
+    delay: 10,
+    iterations: 5,
+    direction: 'alternate',
+    fill: 'forwards'
+});*/
